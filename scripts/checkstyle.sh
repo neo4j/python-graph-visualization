7c842a011f128772bc518554f010f1c124112287
Florentin Dörre <florentin.dorre@neo4j.com>
2025-10-26 22:10:37 +0100

    Install checkstyle deps via uv pip
    hoping to resolve dependency resolution error for ruff

--- scripts/checkstyle.sh
+++ scripts/checkstyle.sh
@@ -7,7 +7,7 @@ set -o pipefail
 
 python -m ruff check .
 python -m ruff format --check .
-mypy --config-file "${GIT_ROOT}/python-wrapper/pyproject.toml" .
+python -m mypy --config-file "${GIT_ROOT}/python-wrapper/pyproject.toml" .
 
 
 if [ "${SKIP_NOTEBOOKS:-false}" == "true" ]; then
